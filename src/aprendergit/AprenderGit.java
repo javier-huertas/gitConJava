805e20045531084384d2e59a2dda015e5a1cb908
Javier <huertasmedrano@gmail.com>
2022-07-30 02:48:57 -0500

    Segunda modificacion de la aplicacion

--- src/aprendergit/AprenderGit.java
+++ src/aprendergit/AprenderGit.java
@@ -5,6 +5,7 @@ public class AprenderGit {
 
     public static void main(String[] args) {
         System.out.println("Huertas Medrano Javier");
+        System.out.println("Segundo cambio Javier Huertas");
     }
     
 }
