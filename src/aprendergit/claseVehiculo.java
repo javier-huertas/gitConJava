129161daa03f38b0dec4e531960ecfa0acdcd5b5
Javier <huertasmedrano@gmail.com>
2022-07-30 02:59:20 -0500

    Se agrego la variable numeroDeTimones y sus metodos set y get

--- src/aprendergit/claseVehiculo.java
+++ src/aprendergit/claseVehiculo.java
@@ -2,5 +2,13 @@
 package aprendergit;
  
 public class claseVehiculo {
+    private int numeroDeTimones;
     
+    public int getNumeroDeTimones(){
+       return this.numeroDeTimones;
+    }
+    public void serNumeroDeTimones(int NumeroDeTiomnes){
+        this.numeroDeTimones = NumeroDeTiomnes;
+    }
+            
 }
