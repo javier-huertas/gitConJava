cb0ecc8e3d275e6b6590450d76966e6a025e9550
Javier <huertasmedrano@gmail.com>
2022-07-30 03:13:49 -0500

    Se agregaron los parametros numeroRetrovisores y numeroDePuertas en las clases moto y carro respectivamente

--- src/aprendergit/Carro.java
+++ src/aprendergit/Carro.java
@@ -2,5 +2,14 @@
 package aprendergit;
  
 public class Carro extends claseVehiculo{
+    private int numeroDePuertas;
+
+    public int getNumeroDePuertas() {
+        return numeroDePuertas;
+    }
+
+    public void setNumeroDePuertas(int numeroDePuertas) {
+        this.numeroDePuertas = numeroDePuertas;
+    }
     
 }
--- src/aprendergit/Moto.java
+++ src/aprendergit/Moto.java
@@ -2,5 +2,15 @@
 package aprendergit;
  
 public class Moto extends claseVehiculo{
+    private int numeroRetrovisores;
+
+    public int getNumeroRetrovisores() {
+        return numeroRetrovisores;
+    }
+
+    public void setNumeroRetrovisores(int numeroRetrovisores) {
+        this.numeroRetrovisores = numeroRetrovisores;
+    }
+    
     
 }
